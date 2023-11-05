661490decafaf84c588065811de97d11583e1b8d
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-11-05 18:22:10 +0800

    dev: [General] Add component NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <div>Test 1</div>
+            <div>Test 2</div>
+        </div>
+    )
+}
+
+export default NavBar;
