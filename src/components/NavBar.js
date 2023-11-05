451f0a78ada1980821c483dc8f8e48e4ce03ebee
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-11-05 18:29:58 +0800

    dev: [General] Add tailwindcss

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,9 @@ function NavBar() {
         <div className="NavBar">
             <div>Test 1</div>
             <div>Test 2</div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
