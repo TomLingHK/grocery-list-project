ac73449ea9c69962f9754d1d3677752ce5e4d370
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-11-06 21:51:39 +0800

    dev: [General] Add scss

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
+import './NavBar.scss';
+import NavButton from '../NavButton/NavButton';
+
 function NavBar() {
     return (
         <div className="NavBar">
             <div>Test 1</div>
             <div>Test 2</div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <div>Test 3</div>
+            <NavButton>Test</NavButton>
         </div>
     )
 }
