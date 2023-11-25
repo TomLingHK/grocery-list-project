63966418f7e138c528edff0c605bbed6601e4bb6
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-11-25 18:31:35 +0800

    dev: [General] Add component Authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import data from "./data/data.json";
 
+import ThemeContext from "./context/ThemeContext";
+
 import NavBar from "./components/NavBar/NavBar";
 import TableContent from "./components/TableContent/TableContent";
 import ThemeButton from "./components/ThemeButton/ThemeButton";
-
-import ThemeContext from "./context/ThemeContext";
+import Authentication from "./components/Authentication/Authentication";
 
 function App() {
     const [selected, setSelected] = useState(0);
@@ -26,6 +27,7 @@ function App() {
                 <NavBar items={data} handleClick={ handleClick }></NavBar>
                 <TableContent content={ data.nav[selected].content }></TableContent>
                 <ThemeButton setTheme={ setTheme }></ThemeButton>
+                <Authentication/>
             </div>
         </ThemeContext.Provider>
     );
---
+++ src/components/Authentication/Authentication.jsx
@@ -0,0 +1,39 @@
+import { auth, googleProvider } from "../../config/firebase-config";
+import { signInWithPopup, signOut } from "firebase/auth";
+
+import "./Authentication.scss";
+
+function Authentication() {
+    console.log(auth?.currentUser?.email);
+
+    async function signInWithGoogle() {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function logOut() {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            {/* TODO change UI after clicking login */}
+            <button id="googleSignInBtn" className="authbtn" onClick={signInWithGoogle}>
+                Sign In With Google
+            </button>
+            {/* TODO change UI after clicking logout */}
+            <button id="logOutBtn" className="authbtn" onClick={logOut}>
+                Sign out
+            </button>
+        </div>
+    )
+}
+
+export default Authentication;
