adc2ed1095809706897bc22d12331e0a5002b4a6
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-11-25 18:03:16 +0800

    dev: [General] Install firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1w5H1JzUR30fh__eSCC2cuixAJpezJMo",
+  authDomain: "record-project-7007a.firebaseapp.com",
+  projectId: "record-project-7007a",
+  storageBucket: "record-project-7007a.appspot.com",
+  messagingSenderId: "440624764611",
+  appId: "1:440624764611:web:b1bb72de223669ddf1b476"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
