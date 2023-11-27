fd975cbbd560896581898b3a6adc92e1a8c345e1
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-11-27 21:40:45 +0800

    dev: [General] Add firebase features - Add entry to database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import data from "./data/data.json";
 import { db } from "./config/firebase-config";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
 import ThemeContext from "./context/ThemeContext";
 
@@ -15,27 +15,48 @@ function App() {
     const [theme, setTheme] = useState('light');
     const [navList, setNavList] = useState([]);
 
+    // New Nav States
+    const [newNavTitle, setNewNavTitle] = useState("");
+    const [newNavIndex, setNewNavIndex] = useState(0);
+    const [newNavIsTesting, setNewNavIsTesting] = useState(false);
+
     const navListCollectionRef = collection(db, "navBar");
 
     const className = "App " + theme;
     
-    useEffect(() => {
-        const getNavList = async () => {
-            try {
-                const data = await getDocs(navListCollectionRef);
-                const filteredData = data.docs.map((doc) => ({
-                    ...doc.data(),
-                    id: doc.id,
-                }))
-                setNavList(filteredData);
-            } catch (error) {
-                console.error(error);
-            }
+    
+    const getNavList = async () => {
+        try {
+            const data = await getDocs(navListCollectionRef);
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+            }))
+            setNavList(filteredData);
+        } catch (error) {
+            console.error(error);
         }
+    }
 
+    useEffect(() => {
         console.log("App rendered: ");
         getNavList();
-    }, [])
+    }, []);
+
+    const onSubmitNav = async () => {
+        try {
+            await addDoc(navListCollectionRef, {
+                title: newNavTitle, 
+                index: newNavIndex, 
+                testing: newNavIsTesting
+            });
+            
+            getNavList();
+        } catch (error) {
+            console.error(error);
+        }
+
+    };
 
     function handleClick(children) {
         setSelected(children.index);
@@ -48,6 +69,23 @@ function App() {
                 <TableContent content={ data.nav[selected].content }></TableContent>
                 <ThemeButton setTheme={ setTheme }></ThemeButton>
                 <Authentication/>
+                <div>
+                    <input 
+                        placeholder="Title..." 
+                        onChange={(e) => setNewNavTitle(e.target.value)}
+                    />
+                    <input 
+                        placeholder="Index..."
+                        onChange={(e) => setNewNavIndex(e.target.value)}
+                    />
+                    <input 
+                        type="checkbox" 
+                        checked={newNavIsTesting} 
+                        onChange={(e) => setNewNavIsTesting(e.target.checked)}
+                    />
+                    <label>Is Testing</label>
+                    <button onClick={onSubmitNav}>Add New Nav</button>
+                </div>
                 {navList.map((nav) => (
                     <div>
                         <h1 style={{ color: nav.testing ? "red" : "green" }}>{ nav.title }</h1>
