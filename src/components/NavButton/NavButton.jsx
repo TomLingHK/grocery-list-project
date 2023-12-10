107627336b78ddbdf0b90c92965f81716b8c7529
TOM-GIGABYTE\user <bxtom0@gmail.com>
2023-12-10 18:37:45 +0800

    dev: [General] Add deleteButton and edit functionality on navButton

--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
@@ -12,14 +12,28 @@ function NavButton({ children, index, handleClick }) {
     const theme = useContext(ThemeContext);
     const className = "NavButton " + theme + " " + isTesting;
 
+    const [isEditing, setIsEditing] = useState(false);
+    const [title, setTitle] = useState(children.title);
+
     return (
         <div className={ className }>
-            <div className="NavButtonTitle" onClick={ () => { handleClick(index) }}>
-                { children.title }
-            </div>
-            <div className="EditButtons">
-                <FontAwesomeIcon className="EditButton_normal" icon={icon({name: 'pen-to-square', style: 'regular'})} />
-                <FontAwesomeIcon className="EditButton_hover" icon={icon({name: 'pen-to-square', style: 'solid'})} />
+            {
+                isEditing
+                ?
+                    <input type="text" value={ title } onChange={(e) => setTitle(e.target.value)} />
+                :
+                <>
+                    <div className="NavButtonTitle" onClick={ () => { handleClick(index) }}>
+                        { children.title }
+                    </div>
+                    <div className="EditButtons" onClick={ () => {setIsEditing(true)}}>
+                        <FontAwesomeIcon className="EditButton_normal" icon={icon({name: 'pen-to-square', style: 'regular'})} />
+                        <FontAwesomeIcon className="EditButton_hover" icon={icon({name: 'pen-to-square', style: 'solid'})} />
+                    </div>
+                </>
+            }
+            <div className="DeleteButtons">
+                <FontAwesomeIcon className="DeleteButton" icon={icon({name: 'circle-xmark', style: 'solid'})} />
             </div>
         </div>
     )
